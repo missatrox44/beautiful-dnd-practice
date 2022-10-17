47cf61e2e9483210d5b309ff93be41eccd193feb
Sara Baqla <missatrox44@gmail.com>
2022-10-17 15:23:26 -0500

    set up folder structure with initial code - broken

---
+++ README.md
@@ -0,0 +1,1 @@
+# practice beautiful dnd 
---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'reset-css';
+import { DragDropContext } from 'react-beautiful-dnd';
+import initialData from './initial-data';
+import Column from './column';
+
+class App extends React.Component {
+  state = initialData;
+//only required callback
+  onDragEnd = result => {
+    //TODO: reorder our column
+  }
+
+  render() {
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        {this.state.columnOrder.map(columnId => {
+          const column = this.state.columns[columnId];
+          const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+          return <Column key={column.id} column={column} tasks={tasks} />;
+        })}
+      </DragDropContext>
+    );
+  }
+}
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

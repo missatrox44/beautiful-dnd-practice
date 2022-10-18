b4778aa25ad1ab5196130f4b40726b99e424bbea
Sara Baqla <missatrox44@gmail.com>
2022-10-18 14:38:27 -0500

    draggables not persisting in column, state bug

--- src/index.js
+++ src/index.js
@@ -8,9 +8,41 @@ import Column from './column';
 class App extends React.Component {
   state = initialData;
   //only required callback
-  onDragEnd = result => {
-    //TODO: reorder our column
-  }
+  // onDragEnd = result => {
+  //   //info interested in from result object
+  //   const { destination, source, draggableId } = result;
+
+  //   //if no destination, nothing to do
+  //   if (!destination) {
+  //     return;
+  //   }
+  //   //check if location of the draggable changed
+  //   //user dropped item in same place where it started
+  //   if (destination.droppableId === source.droppableId && destination.index === source.index) {
+  //     return;
+  //   }
+
+  //   //have items persist when reordered
+  //   const column = this.state.columns[source.droppableId];
+  //   const newTaskIds = Array.from(column.taskIds);
+  //   newTaskIds.splice(source.index, 1);
+  //   newTaskIds.splice(destination.index, 0, draggableId);
+
+  //   const newColumn = {
+  //     ...column,
+  //     taskIds: newTaskIds,
+  //   };
+
+  //   const newState = {
+  //     ...this.state,
+  //     columns: {
+  //       ...this.state.columns,
+  //       [newColumn.id]: newColumn,
+  //     },
+  //   };
+
+  //   this.setState(newState);
+  // };
 
   render() {
     return (

8ebace54a9ad81cfb2b5bcfcdeca027ea131dee2
Sara Baqla <missatrox44@gmail.com>
2022-10-19 12:32:50 -0500

    almost works, one task not draggle - bug

--- src/index.js
+++ src/index.js
@@ -1,68 +1,167 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import 'reset-css';
-import { DragDropContext } from 'react-beautiful-dnd';
-import initialData from './initial-data';
-import Column from './column';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd'
+import styled from 'styled-components'
 
-class App extends React.Component {
-  state = initialData;
-  //only required callback
-  // onDragEnd = result => {
-  //   //info interested in from result object
-  //   const { destination, source, draggableId } = result;
+import initialData from './initial-data'
+import Column from './column'
 
-  //   //if no destination, nothing to do
-  //   if (!destination) {
-  //     return;
-  //   }
-  //   //check if location of the draggable changed
-  //   //user dropped item in same place where it started
-  //   if (destination.droppableId === source.droppableId && destination.index === source.index) {
-  //     return;
-  //   }
+const Container = styled.div`
+  display:flex;
+`
 
-  //   //have items persist when reordered
-  //   const column = this.state.columns[source.droppableId];
-  //   const newTaskIds = Array.from(column.taskIds);
-  //   newTaskIds.splice(source.index, 1);
-  //   newTaskIds.splice(destination.index, 0, draggableId);
-
-  //   const newColumn = {
-  //     ...column,
-  //     taskIds: newTaskIds,
-  //   };
-
-  //   const newState = {
-  //     ...this.state,
-  //     columns: {
-  //       ...this.state.columns,
-  //       [newColumn.id]: newColumn,
-  //     },
-  //   };
-
-  //   this.setState(newState);
-  // };
-
+class InnerList extends React.PureComponent {
   render() {
-    return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
-        {this.state.columnOrder.map(columnId => {
-          const column = this.state.columns[columnId];
-          const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
-
-          return <Column key={column.id} column={column} tasks={tasks} />;
-        })}
-      </DragDropContext>
-    );
+    const { column, taskMap, index } = this.props;
+    const tasks = column.taskIds.map(taskId => taskMap[taskId]);
+    return <Column column={column} tasks={tasks} index={index} />;
   }
 }
 
+class App extends React.Component {
+  state = initialData
 
+  onDragStart = (start, provided) => {
+    provided.announce(`You have lifted the task in position ${start.source.index + 1}`,
+    );
+  };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  onDragUpdate = (update, provided) => {
+    const message = update.destination
+      ? `You have moved the task to position ${update.destination.index + 1}`
+      : `You are currently not over a droppable area`;
+
+    provided.announce(message);
+  };
+  //only required callback
+  onDragEnd = (result, provided) => {
+    const message = result.destination
+      ? `You have moved the task from position
+          ${result.source.index + 1} to ${result.destination.index + 1}`
+      : `The task has been returned to its starting position of
+            ${result.source.index + 1}`;
+
+    provided.announce(message);
+//info interested in from result object
+    const { destination, source, draggableId, type } = result
+//if no destination -> nothing to do
+    if (!destination) {
+      return
+    }
+//check if location of the draggable changed
+//user dropped item in same place where it started
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return
+    }
+
+    if (type === 'column') {
+      const newColumnOrder = Array.from(this.state.columnOrder);
+      newColumnOrder.splice(source.index, 1);
+      newColumnOrder.splice(destination.index, 0, draggableId);
+
+      const newState = {
+        ...this.state,
+        columnOrder: newColumnOrder,
+      };
+      this.setState(newState);
+      return;
+    }
+
+    const start = this.state.columns[source.droppableId]
+    const finish = this.state.columns[destination.droppableId]
+
+    if (start === finish) {
+      const newTaskIds = Array.from(start.taskIds)
+      newTaskIds.splice(source.index, 1)
+      newTaskIds.splice(destination.index, 0, draggableId)
+
+      const newColumn = {
+        ...start,
+        taskIds: newTaskIds
+      }
+
+      const newState = {
+        ...this.state,
+        columns: {
+          ...this.state.columns,
+          [newColumn.id]: newColumn
+        }
+      }
+
+      this.setState(newState)
+      return
+    }
+
+    // Moving from one list to another
+    const startTaskIds = Array.from(start.taskIds)
+    startTaskIds.splice(source.index, 1)
+    const newStart = {
+      ...start,
+      taskIds: startTaskIds
+    }
+
+    const finishTaskIds = Array.from(finish.taskIds)
+    finishTaskIds.splice(destination.index, 0, draggableId)
+    const newFinish = {
+      ...finish,
+      taskIds: finishTaskIds
+    }
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newStart.id]: newStart,
+        [newFinish.id]: newFinish
+      }
+    }
+    this.setState(newState)
+  }
+
+  render() {
+    return (
+      <DragDropContext
+        onDragStart={this.onDragStart}
+        onDragUpdate={this.onDragUpdate}
+        onDragEnd={this.onDragEnd}>
+        <Droppable
+          droppableId="all-columns"
+          direction="horizontal"
+          type="column"
+        >
+          {provided => (
+            <Container
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {this.state.columnOrder.map((columnId, index) => {
+                const column = this.state.columns[columnId]
+                /* eslint-disable no-unused-vars */
+                const tasks = column.taskIds.map(
+                  taskId => this.state.tasks[taskId]
+                );
+                /* eslint-enable no-unused-vars */
+                return (
+                  <InnerList
+                    key={column.id}
+                    column={column}
+                    taskMap={this.state.tasks}
+                    index={index}
+                  />
+                );
+              })}
+              {provided.placeholder}
+            </Container>
+          )}
+        </Droppable>
+      </DragDropContext>
+    )
+  }
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
--- src/task.js
+++ src/task.js
@@ -1,29 +1,42 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Draggable } from 'react-beautiful-dnd';
+import React from 'react'
+import styled from 'styled-components'
+import { Draggable } from 'react-beautiful-dnd'
 
 const Container = styled.div`
   border: 1px solid lightgrey;
   border-radius: 2px;
   padding: 8px;
   margin-bottom: 8px;
-  background-color: #088F8F;
-`;
+  transition: background-color 0.2s ease;
+  background-color: ${props =>
+    props.isDragDisabled
+      ? 'lightgrey'
+      : props.isDragging
+        ? 'lavender'
+        : 'white'};
+`
 
 export default class Task extends React.Component {
   render() {
+    const isDragDisabled = this.props.task.id === 'task-1'
     return (
-    <Draggable draggableId={this.props.task.id} index={this.props.index}>
-      {provided => (
-        <Container
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-        >
-          {this.props.task.content}
-        </Container>
-      )}
-    </Draggable>
-    );
+      <Draggable
+        draggableId={this.props.task.id}
+        index={this.props.index}
+        isDragDisabled={isDragDisabled}
+      >
+        {(provided, snapshot) => (
+          <Container
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef}
+            isDragging={snapshot.isDragging}
+            aria-roledescription="Press space bar to lift the task"
+          >
+            {this.props.task.content}
+          </Container>
+        )}
+      </Draggable>
+    )
   }
 }
